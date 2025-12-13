9407c11dacf15a30578ed84f10cab5e8ff736d20
cairesdev <joaocaires.dev@gmail.com>
2025-12-12 22:34:07 -0300

    API

--- src/constants/enverioments.ts
+++ src/constants/enverioments.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.API_URL || "http://localhost:3333/";
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3333/";
 export const NOME_SISTEMA =
   process.env.NEXT_PUBLIC_NOME_SISTEMA || "Alimentação Online";
 
