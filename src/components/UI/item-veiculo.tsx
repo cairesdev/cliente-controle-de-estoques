068e7dc6a22f08599a82561c8f5002a9a8ae21b9
cairesdev <joaocaires.dev@gmail.com>
2026-01-21 09:23:07 -0300

    Veiculo

--- src/components/UI/item-veiculo.tsx
+++ src/components/UI/item-veiculo.tsx
@@ -1,15 +1,26 @@
 import { Veiculo } from "@/types/entidade";
 import styles from "@/styles/components/item_catalogo.module.css";
+import { FaCarSide, FaTag } from "react-icons/fa";
 
 export default function ItemVeiculo({ item }: { item: Veiculo }) {
   return (
     <div className={styles.container_veiculo}>
-      <p className={styles.nome}>{item.nome}</p>
-      <p className={styles.modelo}>
-        {item.marca} / {item.modelo}
-      </p>
-      <p className={styles.placa}>{item.placa}</p>
-      <p className={styles.status}>{item.status}</p>
+      <div className={styles.placa}>
+        <FaTag size={14} />
+        <span>{item.placa}</span>
+      </div>
+
+      <div className={styles.info}>
+        <p className={styles.nome}>
+          <FaCarSide size={14} />
+          {item.nome}
+        </p>
+        <p className={styles.modelo}>
+          {item.marca} / {item.modelo}
+        </p>
+      </div>
+
+      <div className={styles.status}>{item.status}</div>
     </div>
   );
 }
